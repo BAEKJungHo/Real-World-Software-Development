06caab61abc29c342851f63bdf1b3bec3aad14f6
BAEKJungHo <designjava@naver.com>
2021-01-25 22:57:14 +0900

    Create new project

---
+++ src/main/java/com/weave/rwsd/RwsdApplication.java
@@ -0,0 +1,13 @@
+package com.weave.rwsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwsdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weave/rwsd/RwsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weave.rwsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
